6fac0d9a98cd1f0ac91d887f96dae370f2e24211
SoulMind01 <jackyrobot01@gmail.com>
2025-05-08 16:27:18 -0700

    Use general attribute count function by date

--- src/components/statProcessor.js
+++ src/components/statProcessor.js
@@ -1,23 +1,23 @@
-function processIssues(stats)
+// Count date for an attribute
+// Assuming the attribute has a `created_at` field with `YYYY-MM-DDTHH:MM:SSZ` form
+export function processAttribute(stats, attributeName)
 {
   try
   {
-    const issues = stats.issues;
-    if (!issues) throw new Error("Stat doesn't have issues field");
-    // Sort the issues by date
-    const sortedIssues = issues.map(x => x.created_at.slice(5, 10)).sort();
-    const issueCountsByDate = sortedIssues.reduce((acc, date) =>
+    const attribute = stats[attributeName];
+    if (!attribute) throw new Error(`Stat doesn't have the attribute: ${attributeName}`);
+    const sortedAttribute = attribute.map(x => x.created_at.slice(5, 10)).sort();
+    const attributeCountsByDate = sortedAttribute.reduce((acc, date) =>
     {
       acc[date] = (acc[date] || 0) + 1;
       return acc;
     }, {})
-    const issueCountArray = Object.entries(issueCountsByDate).map(([date, count]) => ({ date, count }));
-    return issueCountArray;
+    const attributeCountArray = Object.entries(attributeCountsByDate).map(([date, count]) => ({ date, count }));
+    console.log(attributeName, attributeCountArray.length);
+    return attributeCountArray;
   }
   catch (exception)
   {
     console.log(exception);
   }
 }
-
-export default processIssues;
